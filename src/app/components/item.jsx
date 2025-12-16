0213e400c311fa877e96b09905e6704399c8be06
Chaser2123 <chase@LAPTOP-EGEVNV8U.localdomain>
2025-12-15 22:08:17 -0700

    added the transition hook, and an example

--- src/app/components/item.jsx
+++ src/app/components/item.jsx
@@ -3,7 +3,7 @@ import { motion } from 'motion/react'
 
 export default function Item() {
     return (
-        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{ duration: 2 }} className="item w-fit p-5 bg-amber-800 border-2 border-amber-500 border-rounded-lg">
+        <motion.div initial={{opacity: 0, translateY: 15 }} animate={{opacity: 1, translateY: 0}} transition={{ duration: 1, type: "spring", bounce: 0.7, ease: "easeInOut" }} className="item w-fit p-5 bg-amber-800 border-2 border-amber-500 border-rounded-lg">
             <h2>Item Title</h2>
             <p>Item Description</p>
         </motion.div>
